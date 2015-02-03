6e646da20a50a715ada5bd13cba59121d5fdfd38
Andrew Moore <admoore9@gmail.com>
2015-02-02 19:31:38 -0600

    Setup skeleton

---
+++ assignments/moore_andrew.assignment-1.02/dungeon.c
@@ -0,0 +1,420 @@
+/*
+ ******************************************************************
+ * Unless otherwise noted, All code provided below was created by *
+ * Professor Jeremy Sheaffer at Iowa State University. To see my  *
+ * solution to making a random dungeon please see:                *
+ * assignments/moore_andrew.assignment-1.01/dungeonGenerator.c    *
+ ******************************************************************
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+#include <sys/time.h>
+#include <string.h>
+
+/* Adds a "room shrinking" phase if it has trouble placing all the rooms. *
+ * This allows rooms to become smaller than are specified in the problem  *
+ * statement as a minimum room size, so strictly speaking, this is not    *
+ * compliant.                                                             */
+
+#define DUNGEON_X              160
+#define DUNGEON_Y              96
+#define MIN_ROOMS              12
+#define MAX_ROOMS              24
+#define ROOM_MIN_X             10
+#define ROOM_MIN_Y             8
+#define ROOM_SEPARATION        5
+#define MAX_PLACEMENT_ATTEMPTS 1000
+
+#define rand_under(numerator, denominator) \
+  (rand() < ((RAND_MAX / denominator) * numerator))
+#define rand_range(min, max) ((rand() % (((max) + 1) - (min))) + (min))
+
+typedef enum dim {
+  dim_x,
+  dim_y,
+  num_dims
+} dim_t;
+
+typedef int16_t pair_t[num_dims];
+
+typedef enum terrain_type {
+  ter_debug,
+  ter_wall,
+  ter_wall_no_room,
+  ter_wall_no_floor,
+  ter_wall_immutable,
+  ter_floor,
+  ter_floor_room,
+  ter_floor_hall,
+  ter_floor_tentative
+} terrain_type_t;
+
+typedef struct room {
+  pair_t position;
+  pair_t size;
+  uint32_t connected;
+  uint8_t exists;
+} room_t;
+
+typedef struct dungeon {
+  uint32_t num_rooms;
+  room_t rooms[MAX_ROOMS];
+  uint8_t map[DUNGEON_Y][DUNGEON_X];
+} dungeon_t;
+
+int point_in_room_p(room_t *r, pair_t p)
+{
+  return ((r->position[dim_x] <= p[dim_x]) &&
+          ((r->position[dim_x] + r->size[dim_x]) > p[dim_x]) &&
+          (r->position[dim_y] <= p[dim_y]) &&
+          ((r->position[dim_y] + r->size[dim_y]) > p[dim_y]));
+}
+
+/* Randomly decides whether to choose the x or the y direction first,   *
+ * then draws the corridor connecting two points with one or two lines. */
+int connect_two_points(dungeon_t *d,
+                       pair_t min_x, pair_t max_x,
+                       pair_t min_y, pair_t max_y)
+{
+  uint32_t i;
+  uint32_t x_first;
+
+  x_first = rand() & 1;
+
+  for (i = min_x[dim_x]; i <= max_x[dim_x]; i++) {
+    if (d->map[x_first ? min_y[dim_y] : max_y[dim_y]][i] == ter_wall) {
+      d->map[x_first ? min_y[dim_y] : max_y[dim_y]][i] = ter_floor_hall;
+    }
+  }
+  for (i = min_y[dim_y]; i <= max_y[dim_y]; i++) {
+    if (d->map[i][x_first ? max_y[dim_x] : min_y[dim_x]] == ter_wall) {
+      d->map[i][x_first ? max_y[dim_x] : min_y[dim_x]] = ter_floor_hall;
+    }
+  }
+
+  return 0;
+}
+
+/* Recursively subdivides the space between two points until some minimum *
+ * closeness is reached, then draws the corridors connecting them.        */
+int connect_two_points_recursive(dungeon_t *d, pair_t e1, pair_t e2)
+{
+  pair_t mid;
+  int16_t *min_x, *min_y, *max_x, *max_y;
+
+  min_x = ((e1[dim_x] < e2[dim_x]) ? e1 : e2);
+  max_x = ((min_x == e1) ? e2 : e1);
+  min_y = ((e1[dim_y] < e2[dim_y]) ? e1 : e2);
+  max_y = ((min_y == e1) ? e2 : e1);
+
+  if ((max_x[dim_x] - min_x[dim_x]) + 
+      (max_y[dim_y] - min_y[dim_y]) < 15
+) {
+    return connect_two_points(d, min_x, max_x, min_y, max_y);
+  }
+
+  mid[dim_x] = rand_range(min_x[dim_x], max_x[dim_x]);
+  mid[dim_y] = rand_range(min_y[dim_y], max_y[dim_y]);
+
+  connect_two_points_recursive(d, e1, mid);
+  connect_two_points_recursive(d, mid, e2);
+
+  return 0;
+}
+
+/* Chooses a random point inside each room and connects them with a *
+ * corridor.  Random internal points prevent corridors from exiting *
+ * rooms in predictable locations.                                  */
+int connect_two_rooms(dungeon_t *d, room_t *r1, room_t *r2)
+{
+  pair_t e1, e2;
+
+  e1[dim_y] = rand_range(r1->position[dim_y],
+                         r1->position[dim_y] + r1->size[dim_y] - 1);
+  e1[dim_x] = rand_range(r1->position[dim_x],
+                         r1->position[dim_x] + r1->size[dim_x] - 1);
+  e2[dim_y] = rand_range(r2->position[dim_y],
+                         r2->position[dim_y] + r2->size[dim_y] - 1);
+  e2[dim_x] = rand_range(r2->position[dim_x],
+                         r2->position[dim_x] + r2->size[dim_x] - 1);
+
+  return connect_two_points_recursive(d, e1, e2);
+}
+
+int connect_rooms(dungeon_t *d)
+{
+  uint32_t i, j;
+  uint32_t dist, dist_tmp;
+  uint32_t connected;
+  uint32_t r1, r2;
+  uint32_t min_con, max_con;
+
+  connected = 0;
+
+  /* Find the closest pair and connect them.  Do this until everybody *
+   * is connected to room zero.  Because of the nature of the path    *
+   * drawing algorithm, it's possible for two paths to cross, or for  *
+   * a path to touch another room, but what this produces is *almost* *
+   * a free tree.                                                     */
+  while (!connected) {
+    dist = DUNGEON_X + DUNGEON_Y;
+    for (i = 0; i < d->num_rooms; i++) {
+      for (j = i + 1; j < d->num_rooms; j++) {
+        if (d->rooms[i].connected != d->rooms[j].connected) {
+          dist_tmp = (abs(d->rooms[i].position[dim_x] -
+                          d->rooms[j].position[dim_x]) +
+                      abs(d->rooms[i].position[dim_y] -
+                          d->rooms[j].position[dim_y]));
+          if (dist_tmp < dist) {
+            r1 = i;
+            r2 = j;
+            dist = dist_tmp;
+          }
+        }
+      }
+    }
+    connect_two_rooms(d, d->rooms + r1, d->rooms + r2);
+    min_con = (d->rooms[r1].connected < d->rooms[r2].connected ?
+               d->rooms[r1].connected : d->rooms[r2].connected);
+    max_con = (d->rooms[r1].connected > d->rooms[r2].connected ?
+               d->rooms[r1].connected : d->rooms[r2].connected);
+    for (connected = 1, i = 1; i < d->num_rooms; i++) {
+      if (d->rooms[i].connected == max_con) {
+        d->rooms[i].connected = min_con;
+      }
+      if (d->rooms[i].connected) {
+        connected = 0;
+      }
+    }
+  }
+
+  /* Now let's add a handful of random, extra connections to create *
+   * loops and a more exciting look for the dungeon.  Use a do loop *
+   * to guarantee that it always adds at least one such path.       */
+
+  do {
+    r1 = rand_range(0, d->num_rooms - 1);
+    while ((r2 = rand_range(0, d->num_rooms - 1)) == r1)
+      ;
+    connect_two_rooms(d, d->rooms + r1, d->rooms + r2);
+  } while (rand_under(1, 2));
+
+  return 0;
+}
+
+int place_room(dungeon_t *d, room_t *r)
+{
+  pair_t p;
+  uint32_t tries;
+  uint32_t qualified;
+
+  for (;;) {
+    for (tries = 0; tries < MAX_PLACEMENT_ATTEMPTS; tries++) {
+      p[dim_x] = rand() % DUNGEON_X;
+      p[dim_y] = rand() % DUNGEON_Y;
+      if (p[dim_x] - ROOM_SEPARATION < 0                      ||
+          p[dim_x] + r->size[dim_x] + ROOM_SEPARATION > DUNGEON_X ||
+          p[dim_y] - ROOM_SEPARATION < 0                      ||
+          p[dim_y] + r->size[dim_y] + ROOM_SEPARATION > DUNGEON_Y) {
+        continue;
+      }
+      r->position[dim_x] = p[dim_x];
+      r->position[dim_y] = p[dim_y];
+      for (qualified = 1, p[dim_y] = r->position[dim_y] - ROOM_SEPARATION;
+           p[dim_y] < r->position[dim_y] + r->size[dim_y] + ROOM_SEPARATION;
+           p[dim_y]++) {
+        for (p[dim_x] = r->position[dim_x] - ROOM_SEPARATION;
+             p[dim_x] < r->position[dim_x] + r->size[dim_x] + ROOM_SEPARATION;
+             p[dim_x]++) {
+          if (d->map[p[dim_y]][p[dim_x]] >= ter_floor) {
+            qualified = 0;
+          }
+        }
+      }
+    }
+    if (!qualified) {
+      if (rand_under(1, 2)) {
+        if (r->size[dim_x] > 2) {
+          r->size[dim_x]--;
+        } else if (r->size[dim_y] > 2) {
+          r->size[dim_y]--;
+        }
+      } else {
+        if (r->size[dim_y] > 2) {
+          r->size[dim_y]--;
+        } else if (r->size[dim_x] > 2) {
+          r->size[dim_x]--;
+        }
+      }
+    } else {
+      for (p[dim_y] = r->position[dim_y];
+           p[dim_y] < r->position[dim_y] + r->size[dim_y];
+           p[dim_y]++) {
+        for (p[dim_x] = r->position[dim_x];
+             p[dim_x] < r->position[dim_x] + r->size[dim_x];
+             p[dim_x]++) {
+          d->map[p[dim_y]][p[dim_x]] = ter_floor_room;
+        }
+      }
+      for (p[dim_x] = r->position[dim_x] - 1;
+           p[dim_x] < r->position[dim_x] + r->size[dim_x] + 1;
+           p[dim_x]++) {
+        d->map[r->position[dim_y] - 1][p[dim_x]]                =
+          d->map[r->position[dim_y] + r->size[dim_y] + 1][p[dim_x]] =
+          ter_wall;
+      }
+      for (p[dim_y] = r->position[dim_y] - 1;
+           p[dim_y] < r->position[dim_y] + r->size[dim_y] + 1;
+           p[dim_y]++) {
+        d->map[p[dim_y]][r->position[dim_x] - 1]                =
+          d->map[p[dim_y]][r->position[dim_x] + r->size[dim_x] + 1] =
+          ter_wall;
+      }
+
+      return 0;
+    }
+  }
+
+  return 0;
+}
+
+int place_rooms(dungeon_t *d)
+{
+  uint32_t i;
+
+  for (i = 0; i < d->num_rooms; i++) {
+    place_room(d, d->rooms + i);
+  }
+
+  return 0;
+}
+
+int make_rooms(dungeon_t *d)
+{
+  uint32_t i;
+
+  for (i = 0; i < MIN_ROOMS; i++) {
+    d->rooms[i].exists = 1;
+  }
+  for (; i < MAX_ROOMS && rand_under(6, 8); i++) {
+    d->rooms[i].exists = 1;
+  }
+  d->num_rooms = i;
+
+  for (; i < MAX_ROOMS; i++) {
+    d->rooms[i].exists = 0;
+  }
+
+  for (i = 0; i < d->num_rooms; i++) {
+    d->rooms[i].size[dim_x] = ROOM_MIN_X;
+    d->rooms[i].size[dim_y] = ROOM_MIN_Y;
+    while (rand_under(7, 8)) {
+      d->rooms[i].size[dim_x]++;
+    }
+    while (rand_under(3, 4)) {
+      d->rooms[i].size[dim_y]++;
+    }
+    /* Initially, every room is connected only to itself. */
+    d->rooms[i].connected = i;
+  }
+
+  return 0;
+}
+
+int empty_dungeon(dungeon_t *d)
+{
+  pair_t p;
+
+  for (p[dim_y] = 0; p[dim_y] < DUNGEON_Y; p[dim_y]++) {
+    for (p[dim_x] = 0; p[dim_x] < DUNGEON_X; p[dim_x]++) {
+      d->map[p[dim_y]][p[dim_x]] = ter_wall;
+      if (p[dim_y] == 0 || p[dim_y] == DUNGEON_Y - 1 ||
+          p[dim_x] == 0 || p[dim_x] == DUNGEON_X - 1) {
+        d->map[p[dim_y]][p[dim_x]] = ter_wall_immutable;
+      }
+    }
+  }
+
+  return 0;
+}
+
+int gen_dungeon(dungeon_t *d)
+{
+  /*
+  pair_t p1, p2;
+
+  p1[dim_x] = rand_range(1, 158);
+  p1[dim_y] = rand_range(1, 94);
+  p2[dim_x] = rand_range(1, 158);
+  p2[dim_y] = rand_range(1, 94);
+  */
+
+  empty_dungeon(d);
+
+  /*
+  connect_two_points_recursive(d, p1, p2);
+  return 0;
+  */
+
+  make_rooms(d);
+  place_rooms(d);
+  connect_rooms(d);
+
+  return 0;
+}
+
+void render_dungeon(dungeon_t *d)
+{
+  pair_t p;
+
+  for (p[dim_y] = 0; p[dim_y] < DUNGEON_Y; p[dim_y]++) {
+    for (p[dim_x] = 0; p[dim_x] < DUNGEON_X; p[dim_x]++) {
+      switch (d->map[p[dim_y]][p[dim_x]]) {
+      case ter_wall:
+      case ter_wall_no_room:
+      case ter_wall_no_floor:
+      case ter_wall_immutable:
+        putchar('#');
+        break;
+      case ter_floor:
+      case ter_floor_room:
+      case ter_floor_hall:
+      case ter_floor_tentative:
+        putchar('.');
+        break;
+      case ter_debug:
+        putchar('*');
+        break;
+      }
+    }
+    putchar('\n');
+  }
+}
+
+/*
+ * Author: Andrew Moore
+ */
+void save_dungeon(dungeon_t *d)
+{
+  printf("saving dungeon...\n");
+}
+
+/*
+ * Author: Andrew Moore
+ */
+void load_dungeon(dungeon_t *d)
+{
+  printf("loading dungeon...\n");
+  gen_dungeon(d);
+}
+
+/*
+ * Author: Andrew Moore
+ */
+int main(int argc, char *argv[])
+{  return 0;
+}
