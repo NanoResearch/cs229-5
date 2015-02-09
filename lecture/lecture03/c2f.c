17b437f6086637028cc02df5c5eb6d408e76b875
Andrew Moore <admoore9@gmail.com>
2015-02-09 14:23:09 -0600

    remove lecture folder

--- lecture/lecture03/c2f.c
+++
@@ -1,6 +0,0 @@
-#include "cf.h"
-
-int f2c(int fahrenheit)
-{
-  return ((fahrenheit - 32) * 5) / 9;
-}
--- lecture/lecture03/cf.h
+++
@@ -1,7 +0,0 @@
-#ifndef CF_H
-# define CF_H
-
-/* int c2f(int celcius);*/
-int f2c(int fahrenheit);
-
-#endif
