d46792caa4f9c43c3933e629478c14f3e233438e
Andrew Moore <admoore9@gmail.com>
2015-02-22 13:40:12 -0600

    setup character struct

--- character.h
+++ character.h
@@ -1,9 +1,28 @@
 #ifndef CHARACTER_H
-# define CHARACTER_H
+#define CHARACTER_H
 
-# include "dungeon.h"
+typedef enum char_type {
+  pc,
+  npc
+} char_type_t;
+
+typedef struct pc {
+  
+} pc_t;
+
+typedef struct npc {
+  uint32_t smart;
+  uint32_t tele;
+} npc_t;
 
 typedef struct character {
+  char_type_t type;
+  pc_t pc;
+  npc_t npc;
+  char symbol;
+  uint8_t speed;
+  uint16_t pos[2];
+  uint32_t alive;
 } character_t;
 
 #endif
