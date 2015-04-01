efb2c0623439126ee1c4fa8bd8be57e67ae6af32
Andrew Moore <admoore9@gmail.com>
2015-03-31 21:51:15 -0500

    setup parse header file and add monster description class

--- parse.h
+++ parse.h
@@ -5,16 +5,28 @@
 extern "C" {
 # endif
 
-struct dungeon_t;
+#define MONSTER_DESCRIPTION_VERSIONING "RLG229 MONSTER DESCRIPTION 1"
 
-void parse_monster_descriptions(dungeon_t *d);
-void print_monster_descriptions(dungeon_t *d);
-void destroy_monster_descriptions(dungeon_t *d);
-
-class monster_description {
-};
+void parse_monster_descriptions();
 
 # ifdef __cplusplus
+
+class monster_description {
+  private:
+    std::string m_name, m_description, m_color, m_speed, m_hp, m_dmg;
+    char m_symbol;
+  public:
+    void set_description(std::string name, std::string description, std::string color, std::string speed, std::string hp, std::string dmg, char symbol) {
+      m_name = name;
+      m_description = description;
+      m_color = color;
+      m_speed = speed;
+      m_hp = hp;
+      m_dmg = dmg;
+      m_symbol = symbol;
+    }
+};
+
 }
 # endif
 
