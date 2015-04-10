2a82aa6dff0ccbd3fb7b105732684b0fcce33b35
Andrew Moore <admoore9@gmail.com>
2015-04-10 17:27:20 -0500

    add assignment-1.08 solution

---
+++ descriptions.h
@@ -0,0 +1,98 @@
+#ifndef DESCRIPTIONS_H
+# define DESCRIPTIONS_H
+
+# include <stdint.h>
+
+typedef struct dungeon dungeon_t;
+
+# ifdef __cplusplus
+
+# include <string>
+# include "dice.h"
+
+extern "C" {
+# endif
+
+uint32_t parse_descriptions(dungeon_t *d);
+uint32_t print_descriptions(dungeon_t *d);
+uint32_t destroy_descriptions(dungeon_t *d);
+
+typedef enum object_type {
+  objtype_no_type,
+  objtype_WEAPON,
+  objtype_OFFHAND,
+  objtype_RANGED,
+  objtype_ARMOR,
+  objtype_HELMET,
+  objtype_CLOAK,
+  objtype_GLOVES,
+  objtype_BOOTS,
+  objtype_RING,
+  objtype_AMULET,
+  objtype_LIGHT,
+  objtype_SCROLL,
+  objtype_BOOK,
+  objtype_FLASK,
+  objtype_GOLD,
+  objtype_AMMUNITION,
+  objtype_FOOD,
+  objtype_WANT,
+  objtype_CONTAINER
+} object_type_t;
+
+# ifdef __cplusplus
+} /* extern "C" */
+
+class monster_description {
+ private:
+  std::string name, description;
+  char symbol;
+  uint32_t color, abilities;
+  dice speed, hitpoints, damage;
+ public:
+  monster_description() : name(),       description(), symbol(0),   color(0),
+                          abilities(0), speed(),       hitpoints(), damage()
+  {
+  }
+  void set(const std::string &name,
+           const std::string &description,
+           const char symbol,
+           const uint32_t color,
+           const dice &speed,
+           const uint32_t abilities,
+           const dice &hitpoints,
+           const dice &damage);
+  std::ostream &print(std::ostream &o);
+};
+
+class object_description {
+ private:
+  std::string name, description;
+  object_type_t type;
+  uint32_t color;
+  dice hit, damage, dodge, defence, weight, speed, attribute, value;
+ public:
+  object_description() : name(),    description(), type(objtype_no_type),
+                         color(0),  hit(),         damage(),
+                         dodge(),   defence(),     weight(),
+                         speed(),   attribute(),   value()
+  {
+  }
+  void set(const std::string &name,
+           const std::string &description,
+           const object_type_t type,
+           const uint32_t color,
+           const dice &hit,
+           const dice &damage,
+           const dice &dodge,
+           const dice &defence,
+           const dice &weight,
+           const dice &speed,
+           const dice &attrubute,
+           const dice &value);
+  std::ostream &print(std::ostream &o);
+};
+
+# endif
+
+#endif
