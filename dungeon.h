ea0bcaa6f3a73d422a27ed3e9162dd6338a3c862
Andrew Moore <admoore9@gmail.com>
2015-04-20 18:29:38 -0500

    fix memory leak?

--- dungeon.h
+++ dungeon.h
@@ -28,7 +28,7 @@ extern "C" {
 #define DUNGEON_SAVE_SEMANTIC  "RLG229"
 #define DUNGEON_SAVE_VERSION   1U
 #define VISUAL_RANGE           30
-#define NUM_OBJECTS            50
+#define NUM_OBJECTS            100
 #define PC_SPEED               10
 #define PC_HP                  20
 #define PC_CARRY               2
